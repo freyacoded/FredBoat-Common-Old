04e9f01fa3fc8981538997264496e13b168a3b9f
Frederikam <frogkr@gmail.com>
2016-09-02 13:07:48 +0200

    Migrated util package

---
+++ util/ExitCodes.java
@@ -0,0 +1,9 @@
+package fredboat.common.util;
+
+public class ExitCodes {
+    
+    public static final int EXIT_CODE_NORMAL = 0;
+    public static final int EXIT_CODE_UPDATE = 20;
+    public static final int EXIT_CODE_RESTART = 21;
+    
+}
