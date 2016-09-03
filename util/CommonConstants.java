d8c434458c4af04c5d97c448fdf0f591a1294036
Frederikam <frogkr@gmail.com>
2016-09-03 13:37:26 +0200

    Refactored constants

---
+++ util/CommonConstants.java
@@ -0,0 +1,13 @@
+package fredboat.commons.util;
+
+public class CommonConstants {
+    
+    public static final boolean IS_BETA = System.getProperty("os.name").toLowerCase().contains("windows");
+    public static final String DEFAULT_BOT_PREFIX = IS_BETA ? "¤" : ";;";
+    
+    public static final String MUSIC_BOT_ID = "150376112944447488";
+    public static final String BETA_BOT_ID = "152691313123393536";
+    public static final String MAIN_BOT_ID = "150376112944447488";
+    public static final String OWNER_ID = "81011298891993088";
+    
+}
